89d6aa05991ea2d34b3bca6973ced78082a41138
Mauro <sassaroli@gmail.com>
2012-03-23 14:36:53 -0300

    First changes

--- Android GitHub Tutorial/src/com/mauro/android/github/tutorial/main.java
+++ Android GitHub Tutorial/src/com/mauro/android/github/tutorial/main.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class main extends Activity {
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. 
+     * 
+     * This is a new comment
+     * 
+     * */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
